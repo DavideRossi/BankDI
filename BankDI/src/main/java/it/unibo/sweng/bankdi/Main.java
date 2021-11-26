6689364f98d8d287671432f6b3278054cbee1c06
rossi <rossi@XPS-13-9310>
2021-11-26 16:53:52 +0100

    Dependencies are now injected

--- BankDI/src/main/java/it/unibo/sweng/bankdi/Main.java
+++ BankDI/src/main/java/it/unibo/sweng/bankdi/Main.java
@@ -3,7 +3,9 @@ package it.unibo.sweng.bankdi;
 public class Main {
 	
 	public static void main(String[] args) {
-		TransferManager transferManager = new TransferManager();
+		RemoteBankOperator remoteBankOperator = new SETRemoteBankOperator();
+		TransactionLogger transactionLogger = new FileTransactionLogger();
+		TransferManager transferManager = new TransferManager(remoteBankOperator, transactionLogger);
 		Account account = new Account(123, 100);
 		transferManager.makeTransfer(account, "ITXYZ42", 50);
 	}
--- BankDI/src/main/java/it/unibo/sweng/bankdi/TransferManager.java
+++ BankDI/src/main/java/it/unibo/sweng/bankdi/TransferManager.java
@@ -1,21 +1,26 @@
 package it.unibo.sweng.bankdi;
 
 public class TransferManager {
-	
+	private RemoteBankOperator bankOperator;
+	private TransactionLogger transactionLogger;
+
+	public TransferManager(RemoteBankOperator bankOperator, TransactionLogger transactionLogger) {
+		super();
+		this.bankOperator = bankOperator;
+		this.transactionLogger = transactionLogger;
+	}
+
 	//transfer from a local account to a remote one
 	public boolean makeTransfer(Account sourceAccount, String destinationAccountCode, double amount) {
-		RemoteBankOperator bankOperator = new SETRemoteBankOperator();
-		TransactionLogger transactionLogger = new FileTransactionLogger();
-
 		if(sourceAccount.getBalance() >= amount) {
-			if(bankOperator.transfer(destinationAccountCode, amount)) {
+			if(this.bankOperator.transfer(destinationAccountCode, amount)) {
 				sourceAccount.withdraw(amount);
-				transactionLogger.logTransaction(
+				this.transactionLogger.logTransaction(
 					String.format("Trasferred %f from %s to %s", 
 					amount, sourceAccount.toString(), destinationAccountCode));
 				return true;
 			} else {
-				transactionLogger.logTransaction(
+				this.transactionLogger.logTransaction(
 					String.format("Failed to trasfer %f from %s to %s", 
 					amount, sourceAccount.toString(), destinationAccountCode));
 				return false;
