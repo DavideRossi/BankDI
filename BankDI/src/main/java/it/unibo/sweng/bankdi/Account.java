cdbbfc4293b0164f6a194f08716d5c476f0a5f26
rossi <rossi@XPS-13-9310>
2021-11-26 17:28:33 +0100

    New dependencies are injected in the constructor

--- BankDI/src/main/java/it/unibo/sweng/bankdi/Account.java
+++ BankDI/src/main/java/it/unibo/sweng/bankdi/Account.java
@@ -1,10 +1,53 @@
 package it.unibo.sweng.bankdi;
 
+import java.util.Optional;
+
 public class Account {
+	private static final int ID_LIMIT = 12345;
+
+	public static class AccountBuilder {
+		private Optional<String> owner = Optional.empty();
+		private Optional<Double> balance = Optional.empty();
+		private Optional<Integer> id = Optional.empty();
+
+		public static AccountBuilder createBuilder() {
+			return new AccountBuilder();
+		}
+		
+		private AccountBuilder() {
+		}
+		
+		public AccountBuilder setOwner(String owner) {
+			this.owner = Optional.of(owner);
+			return this;
+		}
+
+		public AccountBuilder setBalance(double balance) {
+			this.balance = Optional.of(balance);
+			return this;
+		}
+
+		public AccountBuilder setId(int id) {
+			this.id = Optional.of(id);
+			return this;
+		}
+
+		public Account build() {
+			if(this.owner.isEmpty()) {
+				throw new IllegalArgumentException("Owner must be set");
+			}
+			String owner = this.owner.get();
+			int id = this.id.orElse((int)(Math.random() * ID_LIMIT));
+			double balance = this.balance.orElse(.0);
+			return new Account(owner, id, balance);
+		}
+	}
+	private String owner;
 	private double balance;
 	private int id;
 	
-	public Account(int id, double balance) {
+	private Account(String owner, int id, double balance) {
+		this.owner = owner;
 		this.id = id;
 		this.balance = balance;
 	}
@@ -28,6 +71,6 @@ public class Account {
 	
 	@Override
 	public String toString() {
-		return "{Account id:"+this.id+", balance:"+balance+"}";
+		return String.format("{Account id: %d, owner: %s, balance: %f}", this.id, this.owner, this.balance);
 	}
 }
--- BankDI/src/main/java/it/unibo/sweng/bankdi/Main.java
+++ BankDI/src/main/java/it/unibo/sweng/bankdi/Main.java
@@ -6,7 +6,7 @@ public class Main {
 		RemoteBankOperator remoteBankOperator = new SETRemoteBankOperator();
 		TransactionLogger transactionLogger = new FileTransactionLogger();
 		TransferManager transferManager = new TransferManager(remoteBankOperator, transactionLogger);
-		Account account = new Account(123, 100);
+		Account account = Account.AccountBuilder.createBuilder().setOwner("Superpippo").setBalance(100).build();
 		transferManager.makeTransfer(account, "ITXYZ42", 50);
 	}
 }
